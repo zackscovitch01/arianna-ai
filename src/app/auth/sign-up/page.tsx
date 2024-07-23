2c146566a866a74332dcc90f05e1ce18e344b38b
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-07-23 19:55:51 -0400

    onboarding flow

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,20 @@
+import SignUpFormProvider from "@/components/forms/sign-up/form-provider";
+import RegistrationFormStep from "@/components/forms/sign-up/registration-step";
+import { AuthContextProvider } from "@/context/use-auth-context";
+import { FormProvider } from "react-hook-form";
+
+type Props = {};
+const SignUp = (props: Props) => {
+  return (
+    <div className="flex-1 py-36 md:px-16 w-full">
+      <div className="flex flex-col h-full gap-3">
+        <SignUpFormProvider>
+          <div className="flex flex-col gap-3">
+            <RegistrationFormStep></RegistrationFormStep>
+          </div>
+        </SignUpFormProvider>
+      </div>
+    </div>
+  );
+};
+export default SignUp;
---
+++ src/components/forms/sign-up/type-selection-form.tsx
@@ -0,0 +1,37 @@
+import { Field, FieldValues, UseFormRegister } from "react-hook-form";
+import UserTypeCard from "./user-type-card";
+
+type Props = {
+  register: UseFormRegister<FieldValues>;
+  userType: "owner" | "student";
+  setUserType: React.Dispatch<React.SetStateAction<"owner" | "student">>;
+};
+const TypeSlectionForm = ({ register, userType, setUserType }: Props) => {
+  return (
+    <>
+      {" "}
+      <h2 className="text-gravel md:text-4xl font-bold">Create an account</h2>
+      <p className="text-iridium md:text-sm">
+        Tell us about yourself! What do you do? Let's tailor your <br />{" "}
+        experience so it best suits you.
+      </p>
+      <UserTypeCard
+        register={register}
+        setUserType={setUserType}
+        userType={userType}
+        value="owner"
+        title="I own a business"
+        text="Setting up my account for my company"
+      />
+      <UserTypeCard
+        register={register}
+        setUserType={setUserType}
+        userType={userType}
+        value="student"
+        title="I am a visitor"
+        text="Looking to learn more about the platform"
+      />
+    </>
+  );
+};
+export default TypeSlectionForm;
